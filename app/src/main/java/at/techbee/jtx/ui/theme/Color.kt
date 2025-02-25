a43049d0139c8a24f6c43e51128b2d2a42d0f235
dmytrovoytko <dmytro.voytko@gmail.com>
2025-02-25 12:04:01 +0200

    Theme colors: blueish palette instead of greenish

--- app/src/main/java/at/techbee/jtx/ui/theme/Color.kt
+++ app/src/main/java/at/techbee/jtx/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package at.techbee.jtx.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+/*
 val md_theme_light_primary = Color(0xFF526600)
 val md_theme_light_onPrimary = Color(0xFFffffff)
 val md_theme_light_primaryContainer = Color(0xFFd3ed7e)
@@ -29,7 +30,41 @@ val md_theme_light_inverseOnSurface = Color(0xFFf3f1e9)
 val md_theme_light_inverseSurface = Color(0xFF30312b)
 val md_theme_light_inversePrimary = Color(0xFFb8d166)
 val md_theme_light_shadow = Color(0xFF000000)
+*/
 
+// Blueish colors (mostly from Thunderbird) // + colors in RGB to see highlights
+
+// val md_theme_light_primary = Color(0xFF004F9B) // 0x004F9B
+val md_theme_light_primary = Color(0xFF0057B7) // 0x0057B7
+val md_theme_light_onPrimary = Color(0xFFffffff) // 0xffffff
+val md_theme_light_primaryContainer = Color(0xFF1373D9) // 0x1373D9
+val md_theme_light_onPrimaryContainer = Color(0xFFffffff) // 0xffffff
+val md_theme_light_secondary = Color(0xFF003D75) // 0x003D75
+val md_theme_light_onSecondary = Color(0xFFffffff) // 0xffffff
+val md_theme_light_secondaryContainer = Color(0xFF2E61A0) // 0x2E61A0
+val md_theme_light_onSecondaryContainer = Color(0xFFffffff) // 0xffffff
+val md_theme_light_tertiary = Color(0xFF3a665e) // 0x3a665e
+val md_theme_light_onTertiary = Color(0xFFffffff) // 0xffffff
+val md_theme_light_tertiaryContainer = Color(0xFFbdece1) // 0xbdece1
+val md_theme_light_onTertiaryContainer = Color(0xFFffffff) // 0xffffff
+val md_theme_light_error = Color(0xFFba1b1b) // 0xba1b1b
+val md_theme_light_errorContainer = Color(0xFFffdad4) // 0xffdad4
+val md_theme_light_onError = Color(0xFFffffff) // 0xffffff
+val md_theme_light_onErrorContainer = Color(0xFF410001) // 0x410001
+val md_theme_light_background = Color(0xFFfefcf4) // 0xfefcf4
+val md_theme_light_onBackground = Color(0xFF1b1c17) // 0x1b1c17
+val md_theme_light_surface = Color(0xFFfefcf4) // 0xfefcf4
+val md_theme_light_onSurface = Color(0xFF1b1c17) // 0x1b1c17
+val md_theme_light_surfaceVariant = Color(0xFFe3e4d3) // 0xe3e4d3
+val md_theme_light_onSurfaceVariant = Color(0xFF46483c) // 0x46483c
+val md_theme_light_outline = Color(0xFF77786b) // 0x77786b
+val md_theme_light_inverseOnSurface = Color(0xFFf3f1e9) // 0xf3f1e9
+val md_theme_light_inverseSurface = Color(0xFF30312b) // 0x30312b
+val md_theme_light_inversePrimary = Color(0xFFA9C7FF) // 0xA9C7FF
+val md_theme_light_shadow = Color(0xFF000000) // 0x000000
+
+
+/* 
 val md_theme_dark_primary = Color(0xFFb8d166)
 val md_theme_dark_onPrimary = Color(0xFF283500)
 val md_theme_dark_primaryContainer = Color(0xFF3c4d00)
@@ -57,3 +92,34 @@ val md_theme_dark_inverseOnSurface = Color(0xFF1b1c17)
 val md_theme_dark_inverseSurface = Color(0xFFe5e3db)
 val md_theme_dark_inversePrimary = Color(0xFF526600)
 val md_theme_dark_shadow = Color(0xFF000000)
+*/ 
+
+// Blueish colors (mostly from Thunderbird) // + colors in RGB to see highlights
+
+val md_theme_dark_primary = Color(0xFFBEE6FF) // 0xBEE6FF
+val md_theme_dark_onPrimary = Color(0xFF003549) // 0x003549
+val md_theme_dark_primaryContainer = Color(0xFF50C2F8) // 0x50C2F8
+val md_theme_dark_onPrimaryContainer = Color(0xFF002E41) // 0x002E41
+val md_theme_dark_secondary = Color(0xFF96CDFF) // 0x96CDFF
+val md_theme_dark_onSecondary = Color(0xFF003352) // 0x003352
+val md_theme_dark_secondaryContainer = Color(0xFF24A7F7) // 0x24A7F7
+val md_theme_dark_onSecondaryContainer = Color(0xFF001423) // 0x001423
+val md_theme_dark_tertiary = Color(0xFFa1d0c5) // 0xa1d0c5
+val md_theme_dark_onTertiary = Color(0xFF033730) // 0x033730
+val md_theme_dark_tertiaryContainer = Color(0xFF214e46) // 0x214e46
+val md_theme_dark_onTertiaryContainer = Color(0xFFbdece1) // 0xbdece1
+val md_theme_dark_error = Color(0xFFffb4a9) // 0xffb4a9
+val md_theme_dark_errorContainer = Color(0xFF930006) // 0x930006
+val md_theme_dark_onError = Color(0xFF680003) // 0x680003
+val md_theme_dark_onErrorContainer = Color(0xFFffdad4) // 0xffdad4
+val md_theme_dark_background = Color(0xFF1b1c17) // 0x1b1c17
+val md_theme_dark_onBackground = Color(0xFFe5e3db) // 0xe5e3db
+val md_theme_dark_surface = Color(0xFF1b1c17) // 0x1b1c17
+val md_theme_dark_onSurface = Color(0xFFe5e3db) // 0xe5e3db
+val md_theme_dark_surfaceVariant = Color(0xFF46483c) // 0x46483c
+val md_theme_dark_onSurfaceVariant = Color(0xFFc7c8b8) // 0xc7c8b8
+val md_theme_dark_outline = Color(0xFF909283) // 0x909283
+val md_theme_dark_inverseOnSurface = Color(0xFF1b1c17) // 0x1b1c17
+val md_theme_dark_inverseSurface = Color(0xFFe5e3db) // 0xe5e3db
+val md_theme_dark_inversePrimary = Color(0xFF006689) // 0x006689
+val md_theme_dark_shadow = Color(0xFF000000) // 0x000000
